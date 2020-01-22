d0258ff0fb5090f1662d2d22611e15f68859c654
Matt Milton <mattmilton57@gmail.com>
2020-01-22 19:27:02 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -12,8 +12,7 @@ def crew
 end
 
 def greet_crew (crew)
-  greetedcrew = crew.map do {|crew_member| "Hello" + crew_member.} 
-end
+  greetedcrew = crew.map {|crew_member| "Hello" + crew_member.} 
   p greetedcrew
   
 end
